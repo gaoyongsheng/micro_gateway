56806101ed18668585527b85c61b10567433cd64
gys270589787@163.com <gys270589787@163.com>
2021-01-18 00:15:24 +0800

    1.初始化

---
+++ shopping-micro-gateway/src/main/java/com/shopping/micro/gateway/ShoppingMicroGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.micro.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMicroGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMicroGatewayApplication.class, args);
+    }
+
+}
